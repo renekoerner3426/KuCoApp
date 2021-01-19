1c16afadb5cc57785ff3ebfb2dfec93a88fea8e6
Lunarion <chris.froemling@volkswagen.de>
2021-01-19 16:39:43 +0100

    'asdsasdada'

--- VerordnungenImportService/src/main/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/repositories/DecreeEntityRepository.java
+++ VerordnungenImportService/src/main/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/repositories/DecreeEntityRepository.java
@@ -1,10 +1,10 @@
 package com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService.entitites.DecreeEntity;
 
 @Repository
-public interface DecreeEntityRepository extends CrudRepository<DecreeEntity, Long> {
+public interface DecreeEntityRepository extends MongoRepository<DecreeEntity, Long> {
 }
--- VerordnungenImportService/src/test/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/VerordnungenImportServiceApplicationTests.java
+++ VerordnungenImportService/src/test/java/com/vw/fakultaet73/VerordnungenImportService/VerordnungenImportService/VerordnungenImportServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.vw.fakultaet73.VerordnungenImportService.VerordnungenImportService;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
